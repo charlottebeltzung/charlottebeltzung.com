b1de730775c7bcba0ad29089c5c025bd192973ec
Gurvan <gurvanpriem@gmail.com>
2020-04-29 16:50:53 +0200

    initial commit

---
+++ src/js/swap_image.js
@@ -0,0 +1,17 @@
+function makeImageBleu(img) {
+  img.src = img.dataset.src_bleu;
+}
+function makeImageNormal(img) {
+  img.src = img.dataset.src_normal;
+}
+  
+var swapImages = document.getElementsByClassName('swapImage');
+for (var i = 0; i < swapImages.length; i++) {
+  (function () {
+    var swapImage = swapImages[i];
+    swapImage.addEventListener('mouseenter', function() { makeImageNormal(swapImage.getElementsByTagName("img")[0]); }, false);
+    swapImage.addEventListener('touchstart', function() { makeImageNormal(swapImage.getElementsByTagName("img")[0]); }, false);
+    swapImage.addEventListener('mouseleave', function() { makeImageBleu(swapImage.getElementsByTagName("img")[0]); }, false);
+    swapImage.addEventListener('touchend', function() { makeImageBleu(swapImage.getElementsByTagName("img")[0]); }, false);
+  }());
+}
