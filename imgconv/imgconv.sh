169c0e3d4ed132c696fa047153092a08b383ffe9
Gurvan <gurvanpriem@gmail.com>
2020-05-25 19:16:02 +0200

    fait sauvages

--- imgconv/imgconv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# find ./ -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1100\>x {} +
-find ./ -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1100\>x {} +
